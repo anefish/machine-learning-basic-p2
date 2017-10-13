235870b3b5de2552a86bcfa4428d2678c172c96f
anefish <178257297@qq.com>
2017-10-13 15:27:10 +0800

    movie website

---
+++ entertainment_center.py
@@ -0,0 +1,52 @@
+# -*- coding: UTF-8 -*-
+import fresh_tomatoes
+import media
+
+transformers = media.Movie(
+    '变形金刚5：最后的骑士',
+    '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
+    'http://img3.donews.com/uploads/img3/img_pic_1492800778_0.png',
+    'http://120.198.235.230/ugcyd.qq.com/flv/183/138/d0514y123y4.p712.1.mp4'
+)
+
+# print transformers.storyline
+# transformers.show_trailer()
+
+transformers1 = media.Movie(
+    '变形金刚5：最后的骑士',
+    '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
+    'http://img3.donews.com/uploads/img3/img_pic_1492800778_0.png',
+    'http://120.198.235.230/ugcyd.qq.com/flv/183/138/d0514y123y4.p712.1.mp4'
+)
+
+transformers2 = media.Movie(
+    '变形金刚5：最后的骑士',
+    '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
+    'http://img3.donews.com/uploads/img3/img_pic_1492800778_0.png',
+    'http://120.198.235.230/ugcyd.qq.com/flv/183/138/d0514y123y4.p712.1.mp4'
+)
+
+transformers3 = media.Movie(
+    '变形金刚5：最后的骑士',
+    '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
+    'http://img3.donews.com/uploads/img3/img_pic_1492800778_0.png',
+    'http://120.198.235.230/ugcyd.qq.com/flv/183/138/d0514y123y4.p712.1.mp4'
+)
+
+transformers4 = media.Movie(
+    '变形金刚5：最后的骑士',
+    '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
+    'http://img3.donews.com/uploads/img3/img_pic_1492800778_0.png',
+    'http://120.198.235.230/ugcyd.qq.com/flv/183/138/d0514y123y4.p712.1.mp4'
+)
+
+transformers5 = media.Movie(
+    '变形金刚5：最后的骑士',
+    '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
+    'http://img3.donews.com/uploads/img3/img_pic_1492800778_0.png',
+    'http://120.198.235.230/ugcyd.qq.com/flv/183/138/d0514y123y4.p712.1.mp4'
+)
+
+movies = [transformers, transformers1, transformers2, transformers3, transformers4, transformers5]
+
+fresh_tomatoes.open_movies_page(movies)
