b81616aac9945aeb99e2cff61a62da1928bcf9a0
anefish <178257297@qq.com>
2017-10-15 00:14:47 +0800

    update

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,7 +1,9 @@
 # -*- coding: UTF-8 -*-
+# flake8: noqa
 import fresh_tomatoes
 import media
 
+＃ 分别实例化六个Movie类，创建六个对象
 transformers = media.Movie(
     '变形金刚5：最后的骑士',
     '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
@@ -12,41 +14,42 @@ transformers = media.Movie(
 # print transformers.storyline
 # transformers.show_trailer()
 
-transformers1 = media.Movie(
-    '变形金刚5：最后的骑士',
-    '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
-    'http://img3.donews.com/uploads/img3/img_pic_1492800778_0.png',
-    'http://120.198.235.230/ugcyd.qq.com/flv/183/138/d0514y123y4.p712.1.mp4'
+demon = media.Movie(
+    '二代妖精',
+    '身负巨债的动物饲养员袁帅（冯绍峰 饰），为了还清百万巨债，他四处与富家女相亲，却误打误撞爱上来人间报恩的狐妖白纤楚（刘亦菲 饰）。妖怪管理局局长云中鹤为阻止人妖相恋，派魔都小分队洪思聪队长捉拿狐妖白纤楚，袁帅为了寻回爱人狐妖，勇闯妖界……',
+    'http://img5.mtime.cn/CMS/News/2017/07/26/215624.91403037_620X620.jpg',
+    'http://113.96.36.20/om.tc.qq.com/AvRu3fWGzk_7Jc7VGMvDEyuLVWGsF0iNgv2wmO011e3U/z05603lf6dr.p712.1.mp4?sdtfrom=v1103&guid=290c34b3d0275122c7aac9808e87645a&vkey=B3351D2B8F2C67B07BAFD2128AA60FCCE4E9FD925EF9D408FAFB8384A5473B50B9DF44157059C2E0230CDCFE20505C4E9132245EF5B059C8F591B2A590FE50B0B4B27FADEEE31864D3658F01CC424557E0973C7044817185A97A581D471049FAB387725171CDE124383E6505E20B3664B67CAADA8BB445C8&platform=2&locid=23e5c997-68fc-447d-80c7-aa75a0fa2bc4&size=6154197&ocid=184620204'
 )
 
-transformers2 = media.Movie(
-    '变形金刚5：最后的骑士',
-    '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
-    'http://img3.donews.com/uploads/img3/img_pic_1492800778_0.png',
-    'http://120.198.235.230/ugcyd.qq.com/flv/183/138/d0514y123y4.p712.1.mp4'
+union = media.Movie(
+    '正义联盟',
+    '“美国正义联盟”里聚集了DC Comics旗下几乎所有知名的漫画英雄，包括蝙蝠侠、超人、闪电侠等等，众多英雄们聚集在一起除暴安良，保卫地球。',
+    'http://img5.mtime.cn/pi/2016/10/13/083833.92379510_1000X1000.jpg',
+    'http://113.96.35.83/vhot2.qqvideo.tc.qq.com/A4VBhnSGvSFHhka0MIuLKR544doe-hl8bWcYWUzW5khk/n0560zl2e4l.p712.1.mp4?sdtfrom=v1010&guid=6ac1e5c143bc553f7e4c4f46f1ff7a81&vkey=93C4647035FC65491E8E17A7BEEB5771B327F54340DAB8B6700C3E659E9484BDD191D16C5143E71A13E24C628DD62E490A0A8363DCA548E7651891D96013304ADB59A477132176FC690BADD5604C2D3F7BADDF4DEC42AA2E130A5E05EB21E232BD3640EFF0ED2D67B602A56705C569358FB166CE9B6B373C&locid=6626f430-e61d-4a16-b160-8929b5760faa&size=5202774&ocid=251729068'
 )
 
-transformers3 = media.Movie(
-    '变形金刚5：最后的骑士',
-    '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
-    'http://img3.donews.com/uploads/img3/img_pic_1492800778_0.png',
-    'http://120.198.235.230/ugcyd.qq.com/flv/183/138/d0514y123y4.p712.1.mp4'
+numerology = media.Movie(
+    '奇门遁甲',
+    '在一个妖孽横行、乱象频生的时代，世间即将面临惊天浩劫。为了世间安危，江湖上的神秘组织“雾隐门”挺身而出，其成员诸葛青云、铁蜻蜓等个个身怀绝技。新晋捕快刀宜长和身世不明的少女小圆也被意外地卷入这个组织，一行人的奇幻冒险旅程就此展开。',
+    'http://img5.mtime.cn/pi/2016/12/21/092540.78265309_1000X1000.jpg',
+    'http://113.96.154.146/vlive.qqvideo.tc.qq.com/A7Xv2cLusKkJz0y8-N0HoG3bt2HfblUtkuMCq23DzWtU/l0024knyssn.p212.1.mp4?sdtfrom=v1103&guid=290c34b3d0275122c7aac9808e87645a&vkey=353DFEE0A9F240AE798A017DE4EB8C69A8387CBE5D95B2E269223B23C7FE8416F0BEFAF781FA8B4008EE2007C81106ABCCEC8BDE48DEF7BFC7CFFDBF9F93FC4F59415F55797B2AB8A45D4BE0C53D2DF9B592586EAF7F154A6A34081CEA05543FD8413221C351D6C25D220EC2EB22FF2ED88B04E34634F708&platform=2'
 )
 
-transformers4 = media.Movie(
-    '变形金刚5：最后的骑士',
-    '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
-    'http://img3.donews.com/uploads/img3/img_pic_1492800778_0.png',
-    'http://120.198.235.230/ugcyd.qq.com/flv/183/138/d0514y123y4.p712.1.mp4'
+gunner = media.Movie(
+    '天才枪手',
+    '影片根据2014年轰动一时的亚洲考场作弊案改编，讲述了天才学霸利用高智商考场作弊牟取暴利的故事。出生平凡的天才少女Lynn（茱蒂蒙·琼查容苏因 饰）在进入贵族学校后，结识了富二代同学Grace（依莎亚·贺苏汪饰）与Pat（披纳若·苏潘平佑饰），从此开始了考场作弊生涯，与此同时，另一名记忆力极佳的天才学霸Bank（查侬·散顶腾古 饰）发现了Lynn不为人知的“交易”。',
+    'http://img5.mtime.cn/pi/2017/09/25/170943.99514198_1000X1000.jpg',
+    'http://113.96.35.86/om.tc.qq.com/Aum275SzcgbfbAowRk9ardrdH66Oy8UpODoonClOIGCU/l0560wuepv9.p712.1.mp4?sdtfrom=v1103&guid=290c34b3d0275122c7aac9808e87645a&vkey=9197E48A95E3484CA3ACDE6BC057120F86CB22CEE61991535223DFAFD01603BF783BC6898743E4D8DADBF2E34618172CC700434C083EA6828EA12B4AE5AF60B30451C860E0D0EB672941C82229C252C91557C503CA711C1DEA6720310FC14B1ED9E1827D48A31F518276F409144F367A5EC5906CE91B7FF3&platform=2&locid=0a1ebc37-d271-4544-a1e7-963ebb40455c&size=9355572&ocid=1392514220'
 )
 
-transformers5 = media.Movie(
-    '变形金刚5：最后的骑士',
-    '地球陷入毁灭危机，擎天柱失踪，越来越多的汽车人和霸天虎来到地球，前海豹突击队成员威廉·伦诺克斯（ 乔什·杜哈明饰）成为了政府军组建的TRF中的一员，负责绞杀地球上的汽车人——不论好坏。在芝加哥街头，伦诺克斯遇到了凯德·伊格尔（马克·沃尔伯格饰）孤儿伊莎贝拉（伊莎贝拉·莫奈饰）。',
-    'http://img3.donews.com/uploads/img3/img_pic_1492800778_0.png',
-    'http://120.198.235.230/ugcyd.qq.com/flv/183/138/d0514y123y4.p712.1.mp4'
+storm = media.Movie(
+    '全球风暴',
+    '卫星设计师Jake（杰拉德·巴特勒 饰）被指派前往太空，阻止一颗控制地球天气的卫星即将造成的气候灾难，而在执行任务的过程中，他和他关系疏远的哥哥却无意间得知了一个关于刺杀总统的阴谋……',
+    'http://img5.mtime.cn/pi/2017/09/25/110234.97369208_1000X1000.jpg',
+    'http://113.105.167.156/vhot2.qqvideo.tc.qq.com/AxWFEIAYXvyVdHagY15Rk19TcTI25-2JpZfYk7Nh3Uno/o0560xmzl9e.p712.1.mp4?sdtfrom=v1103&guid=290c34b3d0275122c7aac9808e87645a&vkey=8CA2F0551FC4DB709CEE20E6AD6A3CDFC13B3315829C5EA0DDB90140E3BA58EE097B4BC0120F2C2E1AC03D89BA42192939CCECCED2DDB880A0D1D889D7CE8F44A545EA13967B8FB62778FDC3389F18313D3647869E2A5F1AC2C069EB53FE1C6BB33D6BED7ED491FDD5845977EEC5F7CEE0E89E009E79B4A6&platform=2&locid=b16f4b01-397a-42b6-a8ff-80208233c61e&size=1872109&ocid=260820727'
 )
 
-movies = [transformers, transformers1, transformers2, transformers3, transformers4, transformers5]
+# 存储电影对象到列表中
+movies = [transformers, demon, union, numerology, gunner, storm]
 
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,12 +1,22 @@
 import webbrowser
 
 class Movie():
-    """docstring for Movie."""
+    '''定义Movie类，提供初始化函数和播放电影预告的函数'''
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        '''
+        初始化函数
+
+        参数：
+        movie_title -- 电影名称
+        movie_storyline -- 电影简介
+        poster_image -- 电影海报图片路径
+        trailer_youtube -- 电影预告视频路径
+        '''
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        '''播放电影预告视频'''
         webbrowser.open(self.trailer_youtube_url)
